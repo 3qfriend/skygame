bc3f6248f697ac472aa2febab7e0d136f44e9a72
3qfriend <3qfriend@gmail.com>
2014-09-27 11:39:48 +0800

    一些语法错误
    比如__init__ 写成 _int_  , 变量漏字

--- game1.0.0.py
+++ game1.0.0.py
@@ -4,16 +4,16 @@ import random
 import time
 
 class Game:
-    def __int__(self):
+    def __init__(self):
         self.tk=Tk()
         self.tk.title("李翔的游戏1.0.0")
         self.tk.resizable(0,0)
         self.tk.wm_attributes("-topmost",1)
-        self.cavas=Canvas(self.tk,width=500,height=500,highlightthickness=0)
-        self.cavas.pack()
-        self.cavas.update()
+        self.canvas=Canvas(self.tk,width=500,height=500,highlightthickness=0)
+        self.canvas.pack()
+        self.canvas.update()
         self.canvas_width=500
-        self.cavas_height=500
+        self.canvas_height=500
         self.bg=PhotoImage(file="background.gif")
         w =self.bg.width()
         h = self.bg.height()
